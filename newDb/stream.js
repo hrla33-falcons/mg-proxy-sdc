1b29b9725648ceec90ed75a4988f019ed15e7156
Matthew Ginther <mg78856@gmail.com>
2020-01-06 09:41:23 -0800

    Changed connection string

--- newDb/stream.js
+++ newDb/stream.js
@@ -7,7 +7,7 @@ const csvPath = path.join(__dirname, './report.csv');
 var inputFile = path.join(__dirname, './report.csv');
 
 var pool = new Pool({
-    host: 'localhost',
+    host: 'ec2-13-56-115-29.us-west-1.compute.amazonaws.com',
     user: 'ikeaCustomer',
     port: 5432,
     database: 'ikeaproducts'
